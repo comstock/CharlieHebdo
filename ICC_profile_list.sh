e4361d8de2af362e7b82b54fe2276c6f48ab6545
comstock <comstock@users.noreply.github.com>
2016-05-26 20:52:01 -0400

    Update ICC_profile_list.sh

--- ICC_profile_list.sh
+++ ICC_profile_list.sh
@@ -2,11 +2,11 @@
 # Runs Exiftool to generate a listing of icc profiles from source images
 #
 while read line
-do exiftool -icc_profile -b -w icc $line
+do exiftool -icc_profile -b -w icc $line > ../data/icc_profile_list.txt
 done < ../data/imageFilesOnly.txt
 #
 #
 echo "
      ~~~~~ FIN ~~~~~
 "
-# exiftool -icc_profile -b -w icc image.jp
+# exiftool -icc_profile -b -w icc image.jpg
