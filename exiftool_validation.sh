62a7091533b6da4d9f641bcd285459064c12194a
comstock <comstock@users.noreply.github.com>
2016-07-05 15:46:03 -0400

    Update exiftool_validation.sh

--- exiftool_validation.sh
+++ exiftool_validation.sh
@@ -2,13 +2,16 @@
 # run exiftool on a dynamically generated list of image files, and 
 # list the filenames when the searched for metadata string is found.
 
-DIR="/home/comstock/images/"
+DIR="/home/comstock/DIGILAB/TEST/COMSTOCK/CharlieHebdo/LISTS/"
 LIST=$DIR"images.txt"
-SEARCHSTRING="Alpha"
+SEARCHSTRING="tiff"
 
-find $DIR -name *.tif > $LIST
+## TO DO: The images.txt file only gives a relative path to the file. Fix that and the script should find the files.
+
+# find $DIR -name *.tif > $LIST
 
 while read LINE
 do
-    exiftool $LINE | grep $SEARCHSTRING -l
+	exiftool "$LINE" | grep $SEARCHSTRING --only-matching --no-messages -i
+	#echo $LINE
 done < $LIST
