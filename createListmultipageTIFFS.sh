7e06622d1eb196284ce35f4dd531f51d89231023
comstock <comstock@users.noreply.github.com>
2016-07-07 13:21:29 -0400

    not to be trusted

--- createListmultipageTIFFS.sh
+++ createListmultipageTIFFS.sh
@@ -6,6 +6,8 @@
 masterTiffs="/home/comstock/DIGILAB/TEST/COMSTOCK/CharlieHebdo/LISTS/masterTiffs.txt"
 
 while read line; do
+# doesn't work. You cannot count on a tiff having the word "multi-page" in the SubFile Type tag to indicate multipage tiff.
+# I downloaded an example multipage TIFF and it had no SubFile Type value.
 exiftool "$line" | grep -i multi-page; echo "multipage TIFF: $line" >> tiffID_log.txt
 # identify -verbose -format "%p " "$line" >> tiffID_log.txt
 done < $masterTiffs
