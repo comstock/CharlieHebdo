85a4320508815eeddebf09a38424676490af876f
comstock <comstock@users.noreply.github.com>
2016-07-07 18:33:05 -0400

    Create missingFilesLog.py

---
+++ missingFilesLog.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import os,sys
+
+dataDirectory = "path/"
+dataFile = dataDirectory + "file"
+filesNotFound = dataDirectory + "filesNotFound.txt"
+
+f_filesNotFound = open(filesNotFound,'w')
+
+#Your path here e.g. "C:\Program Files\text.txt"
+#if os.path.exists("C:\..."):
+
+if os.path.isfile(dataFile):
+  print "FOUND: " + dataFile
+else:
+  print "MISSING: " + dataFile
+  f_filesNotFound.write(dataFile)
