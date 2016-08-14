96f54a192b60fe9fafb3c24daa77340b7e64705c
comstock <comstock@users.noreply.github.com>
2016-08-14 15:39:10 -0400

    added comments, plus

--- xtra_filenampping2csv.py
+++ xtra_filenampping2csv.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python
 #
-# Take filenameMapping (original filename <tab>  newfilename) text file, strip our directory paths and generate a CSV file that can be impoted into a spreadsheet.
+# takes a text file where each line is path + original filename \tab path + copy of original file, renamed
+# and generates a two column filename mapping file where the left column is the orig filemane and reight column is renamed version of same image file.
+# The file only parses lines that references specific image file types as specified by their file extensions, and ignores lines that do not meet the 
+# image file extension search criteria.
+#
+# Note: to take the renamed file extension and change it to 'jp2', to reflect the file format of the file deposited to DRS, you can use the following
+# command:
+#
+# cat ~/[path]/filenameMapping.txt | sed 's/[a-z]\{3,4\}$/jp2/'
 #
 import os
-import subprocess
-from subprocess import Popen, PIPE
-import PIL
-from PIL import ImageCms
 import re
-import logging
 
-fileList = "/home/comstock/Desktop/filenameMapping.txt"
+fileList = "/home/comstock/Desktop/filenameMapping_jp2.txt"
 twoColumnMappingFile = "/home/comstock/Desktop/twoColumnMappingFile.csv"
 f_fileList = open(fileList, 'r')
 f_twoColumnMappingFile = open(twoColumnMappingFile, 'w')
 
-image_file_types = re.compile('(jpg|jpeg|tif|tiff|png)$',re.IGNORECASE) # These are the file extentions for the image file formats targeted for deposit
+image_file_types = re.compile('(jpg|jpeg|tif|tiff|png)(\s|$)',re.IGNORECASE) # These are the file extentions for the image file formats targeted for deposit.
 
 for line in f_fileList:
-    if re.search(image_file_types,line):
-        head, tail = os.path.split(line)
+    if re.search(image_file_types,line): # if line has targeted image file extension...
+        head, tail = os.path.split(line) # split original filename and renamed filename into variables 'head' and 'tail'.
         left = head ; right = tail
-##        left = re.sub("^\.\/.*\/(.*\.[a-z]{3,4})$","\g<1>",left) 
-        left = re.sub("\t\/.*deliverable.*","",left); ignore,left = os.path.split(left)
-        right = tail
-        print "HEAD:" + head ; print "TAIL: " + tail
-        row = left + "\t" + right
-        f_twoColumnMappingFile.write(row)
+        left = re.sub("\t\/.*deliverable.*","",left) #left = path plus orignal filename with tab and following path-to-renamed file deleted.
+        ignore,left = os.path.split(left) #Split path from original filename where path = 'ignore" var and left = orignal filename.
+##        print "HEAD:" + head ; print "TAIL: " + tail
+        print "ORIGINAL FNAME:\t" + left ; print "RE-WRITTEN FNAME:\t" + right
+        row = left + "\t" + right # define encoding for each row of two column, tab delimited, mapping file.
+        f_twoColumnMappingFile.write(row) #write out file
